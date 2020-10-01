597fd7c1f087e4856d4baf195fc0de672dc8ff66
Gerardo Wacker <32494885+HeladoDeMascarpone@users.noreply.github.com>
2020-09-30 23:06:32 -0300

    Added Home and 404 bases

---
+++ src/routes/NotFound.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class NotFound extends Component {
+    render() {
+        return(
+            <div>
+                <span>404</span>
+            </div>
+        )
+    }
+}
